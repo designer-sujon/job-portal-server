5faee34d62902a01b383c9736739f245633b9eff
designer-sujon <cdnl2018@gmail.com>
2024-12-18 09:19:06 +0600

    added job posted api

--- index.js
+++ index.js
@@ -34,7 +34,14 @@ async function run() {
     const jobsApplicationCollection = client.db('jobPortal').collection('job_applications')
 
     app.get('/jobs', async(req, res)=>{
-        const cursor = jobsCollection.find()
+        const email =  req.query.email
+        let query = {}
+        if(email){
+          query = {hr_email: email}
+        }
+
+
+        const cursor = jobsCollection.find(query)
         const result = await cursor.toArray()
         res.send(result)
     })
@@ -44,6 +51,12 @@ async function run() {
         const query = {_id: new ObjectId(id)}
         const result = await jobsCollection.findOne(query)
         res.send(result)
+    })
+
+    app.post('/jobs', async(req, res)=>{
+      const newJob = req.body
+      const result = await jobsCollection.insertOne(newJob)
+      res.send(result)
     })
 
     // job application api
