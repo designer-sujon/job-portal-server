dcef4639d9702dc485e223cc1e854efefaba87f2
designer-sujon <cdnl2018@gmail.com>
2024-12-19 14:17:00 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
-
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.grtxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -32,6 +34,18 @@ async function run() {
     // jobs related api
     const jobsCollection = client.db('jobPortal').collection('jobs')
     const jobsApplicationCollection = client.db('jobPortal').collection('job_applications')
+
+    //Auth Related APIs
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body
+      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '1h'})
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false
+      })
+      .send({success: true})
+    })
 
     app.get('/jobs', async(req, res)=>{
         const email =  req.query.email
